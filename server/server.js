078cbe81f6b4fd71f03be58e3a8e10e1a58982d6
Tomas Nilsen Nakkerud <74247764+tommenbro@users.noreply.github.com>
2022-07-15 01:13:38 +0200

    new heroku build

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-const server = app.listen(process.env.port || 3000, () => {
-  console.log(`Server started on http://localhost:${server.address().port}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
