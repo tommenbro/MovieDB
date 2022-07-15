592e03aa97353a44394de9f1eccb0a841c2678dc
Tomas Nilsen Nakkerud <74247764+tommenbro@users.noreply.github.com>
2022-07-15 22:52:58 +0200

    heroku fixern

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(express.static("../client/dist/"));
 
 app.use((req, res, next) => {
-  if (req.method === "GET" && !req.path.startsWith("/api/")) {
-    return res.sendFile(path.resolve("../client/dist/index.html"));
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
   } else {
     next();
   }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on http://localhost:${server.address().port}`);
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
